b7775093c13856c07f9a39e9e7b79dac47d7498a
2hyes <36873744+2hyes@users.noreply.github.com>
2020-09-20 20:58:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 app = Flask(__name__)
-model = pickle.load(open('.\decisiontreemodel.pkl', 'rb'))
+model = pickle.load(open('.\AdaBoostmodel.pkl', 'rb'))
 
 @app.route('/')
 def index():
