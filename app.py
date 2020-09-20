31b36afe3598f7a4b3b63590f536d35e541b9e21
2hyes <36873744+2hyes@users.noreply.github.com>
2020-09-20 23:55:04 +0900

    Update: getPredictedAttack func

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
 from flask import Flask,  render_template, request, jsonify
 import pickle
 import numpy as np
+import test
+import preds_mapping
 
 app = Flask(__name__)
-model = pickle.load(open('./AdaBoostmodel.pkl', 'rb'))
+model = pickle.load(open('.\AdaBoostmodel.pkl', 'rb'))
 
 def getPredictedAttack():
-    # result = request.form : 폼 관련 데이터 다루는 곳
-    if reqeust.method == 'POST':
-        #입력받은 여러개의 데이터들을 [np.array]로 저장해서, predict.
-        test_log = request.form['log']
-        test_log = test_log.split(',')
-        test_log = list(map(float, test_log))
-        preds = model.predict([np.array(test_log)])[0]
+    if request.method == 'POST':
+        i = request.form['log']
+        preds = test.test_preds(model)
 
-        ## mapping 정보
-        attack_cat = ['Analysis', 'Backdoor', 'DoS', 'Exploits', 'Fuzzers', 'Generic', 'Normal', 'Reconnaissance', 'Shellcode', 'Worms']
         global prediction
-        prediction = attack_cat[preds]
+        prediction = preds_mapping.attack_category_mapping(preds)
 
-        return prediction
+    return prediction
+
 
 @app.route('/')
 def index():
