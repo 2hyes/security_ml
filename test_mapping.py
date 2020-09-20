5fc940bb5da1e4cac96b1634f25be38ea5e38719
2hyes <36873744+2hyes@users.noreply.github.com>
2020-09-20 23:48:00 +0900

    Delete test_mapping.py

--- test_mapping.py
+++
@@ -1,5 +0,0 @@
-##mapping
-
-def attack_category_mapping(preds):
-    attack_cat = ['Analysis', 'Backdoor', 'DoS', 'Exploits', 'Fuzzers', 'Generic', 'Normal', 'Reconnaissance', 'Shellcode', 'Worms']
-    return attack_cat[preds]
