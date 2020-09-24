8f8b416d6e03919025a5d18c8aa6fc237fe275cc
YEJJINN <45589632+YEJJINN@users.noreply.github.com>
2020-09-24 22:32:36 +0900

    Add files via upload

---
+++ GProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GProject
+//
+//  Created by yejin kim on 2020/09/12.
+//  Copyright © 2020 yejin kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
